47a479a693ff3096e05817fe05d9a1881a25bd31
John <john.a.nilsson@hotmail.com>
2021-03-26 16:12:59 +0100

    Add project files.

---
+++ Inlämningsuppgift1 ASP/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlämningsuppgift1_ASP.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public int OrganizerId { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public string Address { get; set; }
+        public string Date { get; set; }
+        public int SpotsAvailable { get; set; }
+        public List<Attendee> Attendees { get; set; }
+    }
+}
